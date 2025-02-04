c97faccc5d0b11d6c6374361302b1623d4df9c83
ABISHEK P <155505236+Abishek1006@users.noreply.github.com>
2025-02-04 15:34:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/e-od-system/",  // <-- Add this line to set the correct subdirectory path
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000"
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000"
       }
     }
   }
