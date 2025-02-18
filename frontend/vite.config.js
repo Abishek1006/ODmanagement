c537efc4f3edb101afd10c9abc71fc5d1f826eac
Abishek1006 <abishek.nights@gmail.com>
2025-02-19 00:10:06 +0530

    removed base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/e-od-system/",  // <-- Add this line to set the correct subdirectory path
   plugins: [react()],
   server: {
     proxy: {
